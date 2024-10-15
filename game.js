5243a059223e04e1b9f69312461921ebb581a51b
Vyvra <145440118+Vyvra@users.noreply.github.com>
2024-10-15 13:40:52 +0200

    add core gameplay loop
    The dragon now grows with each correct answer, new questions are posed
    when correct answer is given and score is tallied

--- game.js
+++ game.js
@@ -1,3 +1,7 @@
+let level = 0;
+let score = 0;
+let a = 0;
+let b = 0;
 
 function getQuestion(level) {
   let a = Math.floor(Math.random() * 10);
@@ -5,14 +9,14 @@ function getQuestion(level) {
   return [a, b, "+"]
 }
 
-
-
-function poseQuestion(question) {
+function poseQuestion() {
+  let question = getQuestion(level)
   a = question[0]
   b = question[1]
-  operator = question[2]
+  let operator = question[2]
   let posedQuestion = ['How much is ', a, operator, b, '?'].join('')
-  return posedQuestion;
+  const container = document.querySelector('#question')
+  container.innerHTML = posedQuestion
 
 }
 
@@ -20,22 +24,36 @@ function checkAnswer() {
   const answer = document.querySelector('input[id="answer"]');
   if (answer.value == (a + b)) {
     document.getElementById('result').innerHTML = "Correct"
+    score += 1;
+    if (score % 10 == 0) {
+      level += 1;
+    }
   } else {
     document.getElementById('result').innerHTML = "sorry the correct answer was { a + b }"
   }
+  document.getElementById('answer').value = ""
+
+}
+function updateLevelAndScore() {
+  // update score 
+  document.getElementById('score').innerHTML = "score : " + score
+  // update level
+  let dragonSize = ["font-size:", score + 10, "px"].join("")
+  document.getElementById('dragon').setAttribute("style", dragonSize)
+  document.getElementById('level').innerHTML = 'level : ' + level
 }
 
-function game() {
-  question = getQuestion(1)
 
-  const container = document.querySelector('#question')
-  const div = document.createElement('div');
-  div.textContent = poseQuestion(question)
-  container.appendChild(div)
+function gameloop(event) {
+  event.preventDefault()
+  checkAnswer();
+  updateLevelAndScore();
+  poseQuestion();
 }
 
+updateLevelAndScore();
+poseQuestion();
+const btn = document.querySelector('#answerbox');
+btn.addEventListener('submit', gameloop);
 
-const btn = document.querySelector('#submit')
-btn.addEventListener('click', checkAnswer)
-
-game()
+// game()
