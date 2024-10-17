7927d25dd5c13140656746e9ecccc62f70091d42
Vyvra <145440118+Vyvra@users.noreply.github.com>
2024-10-17 23:37:25 +0200

    cleaned up code, changed dragons growth rate.

--- README.md
+++ README.md
@@ -1,14 +1,9 @@
 # Mathdragon
-## A math game where you answer math questions to train your dragon.
+## A math game where you answer math questions to grow your dragon.
 
 This is a small game to help children who are starting math to do math exercises. 
 
-Currently you get asked random addition questions under 20.
+There are five levels of questions. Starting at addition of numbers smaller than five and ending at addition and subtraction of numbers up to 12.
 
 If you answer correctly the dragon grows.
 
-## goals
-[] Make the dragon do more than just grow.
-[] Add other operations; subtraction, multiplication, etc.
-[] Track which questions the user answers wrong, so that extra focus can be put on those questions. 
-[] save progress in a cookie.
--- game.js
+++ game.js
@@ -26,15 +26,24 @@ const LEVELDATA = [
     "maxTerm2": 10,
     "allowNegative": false,
     "operators": ["+", "-"],
+  },
+  {
+    "level": 5,
+    "maxTerm1": 12,
+    "maxTerm2": 12,
+    "allowNegative": false,
+    "operators": ["+", "-"],
   }
 ]
 
+const MAXLEVEL = LEVELDATA.length - 1
+let question
+
 function createQuestion(level) {
   let leveldata
-  let maxlevel = LEVELDATA.length - 1
-  if (level > maxlevel) {
+  if (level > MAXLEVEL) {
     console.log(leveldata)
-    leveldata = LEVELDATA[maxlevel]
+    leveldata = LEVELDATA[MAXLEVEL]
   } else {
     leveldata = LEVELDATA[level]
   }
@@ -43,34 +52,47 @@ function createQuestion(level) {
   let operator = leveldata.operators[Math.floor(Math.random() * leveldata.operators.length)];
   let prompt = [term1, " ", operator, " ", term2].join('')
   let answer = eval(prompt)
+
   if (!leveldata.allowNegative && answer < 0) {
+    // what to do when a question with a negative answer is generated ? just try again, recursively!
+    // I know this is not the cleanest way to do this : P
     retry = createQuestion(level)
     prompt = retry[0]
     answer = retry[1]
   }
-  console.log([prompt, answer])
   return [prompt, answer, operator]
 }
 
-const playerScore = {
+const Player = {
   score: 0,
+  level: 0,
   addScore: function () {
     this.score += 1;
-    if (this.score % 20 == 0) {
-      playerLevel.addLevel()
+    this.level = Math.floor(this.score / 20)
+    if (this.level > MAXLEVEL) {
+      this.level = MAXLEVEL
+      this.saveData()
     }
+  },
+  saveData: function () {
+    localStorage.setItem("score", this.score)
+    localStorage.setItem("level", this.level)
+  },
+  loadData: function () {
+    this.score = Number(localStorage.getItem("score"))
+    this.level = Number(localStorage.getItem("level"))
+  },
+  resetData: function () {
+    this.level = 0
+    this.score = 0
+    this.saveData()
+    updateLevelAndScore()
   }
-
-}
-
-const playerLevel = {
-  level: 0,
-  addLevel: function () { this.level += 1 }
 }
 
 
 function poseQuestion() {
-  let question = createQuestion(playerLevel.level)
+  let question = createQuestion(Player.level)
   let posedQuestion = [question[0], ' = '].join('')
   const container = document.querySelector('#question')
   container.innerHTML = posedQuestion
@@ -81,7 +103,6 @@ function poseQuestion() {
 
   }
   return question
-
 }
 
 function checkAnswer(question) {
@@ -92,7 +113,7 @@ function checkAnswer(question) {
     result.className = 'fadereset'
     result.innerHTML = "&#128293;&#128293;&#9989;&#128293;&#128293;"
     setTimeout(() => { result.className = 'fadeout' }, 300);
-    playerScore.addScore()
+    Player.addScore()
   } else {
     result.className = 'fadereset'
     document.getElementById('result').innerHTML = "&#10062;&#10062;"
@@ -103,18 +124,15 @@ function checkAnswer(question) {
 }
 function updateLevelAndScore() {
   // update score 
-  document.getElementById('score').innerHTML = "Score : " + playerScore.score
-  localStorage.setItem("score", playerScore.score)
-  localStorage.setItem("level", playerLevel.level)
-  // update level
-  let dragonSize = ["font-size:", (playerScore.score * 3) + 20, "px"].join("")
+  document.getElementById('score').innerHTML = "Score : " + Player.score
+  document.getElementById('level').innerHTML = 'Level : ' + Player.level
+  let dragonSize = ["font-size:", (Math.sqrt(Player.score * 800)) + 20, "px"].join("")
   document.getElementById('dragon').setAttribute("style", dragonSize)
-  document.getElementById('level').innerHTML = 'Level : ' + playerLevel.level
 }
 
 
 function gameloop(event) {
-  console.log(localStorage.getItem("score"));
+  // stop page from reloading on submit: 
   event.preventDefault();
   checkAnswer(question);
   updateLevelAndScore();
@@ -122,19 +140,20 @@ function gameloop(event) {
 }
 
 
+function init() {
+  Player.loadData()
+  updateLevelAndScore();
+  question = poseQuestion();
+}
 
 
-savedscore = localStorage.getItem('score')
-playerScore.score += Number(savedscore)
-savedlevel = localStorage.getItem('level')
-playerLevel.level += Number(savedlevel)
-updateLevelAndScore();
-let question = poseQuestion();
+init()
+
 const btn = document.querySelector('#answerbox');
 btn.addEventListener('submit', gameloop);
 
 const reset = document.querySelector('#reset');
-reset.addEventListener("click", () => { playerScore.score = 0; playerLevel.level = 0, updateLevelAndScore(); })
+reset.addEventListener("click", () => { Player.resetData() })
 
 
 
